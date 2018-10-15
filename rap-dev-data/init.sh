5d8fcee4a3650fb4576f91a4fb79133353b175d1
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-10-15 15:09:50 +0200

    elaborate, also add tip for anti-persistence

--- rap-dev-data/init.sh
+++ rap-dev-data/init.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
 # move into data dir
+echo "Moving into data directory..."
 cd regData
+echo ""
+
+if [ ! -f ./payload.tar.gz ]; then
+  echo ""
+  echo "This container is likely based on a previous (initial) state."
+  echo "Hence, data is probably already present. Keep on working!"
+  echo ""
+  echo "To keep your data safe, please consider running"
+  echo "'docker rm -vf \$(docker ps -a -q)' after terminating this container."
+  echo "Then, run this script again next time the container is started."
+  echo ""
+  echo "Exiting."
+  echo ""
+  
+  exit
+fi
+
 
 # unwrap payload
+echo "Unwraping payload"
 tar -xzf payload.tar.gz
+echo ""
 
 # decrypt
+echo "Decrypt payload using your private key"
 ./dec.sh payload.tar.gz.enc ~/.ssh/id_rsa
+echo ""
 
 # extract
+echo "Extract payload" 
 tar -xzf payload.tar.gz
+echo ""
 
 # load
-echo "\nLoading data into database. "
+echo "Loading data into database. "
 mysql -h db -u root -p < dump.sql
+echo ""
 
 # insert Rapporteket config
+echo "Inserting config"
 mkdir -p $R_RAP_CONFIG_PATH
 mv rapbaseConfig.yml $R_RAP_CONFIG_PATH
 mv dbConfig.yml $R_RAP_CONFIG_PATH
+echo ""
 
 # celan-up
+echo "Cleaning up"
 rm -f dump.sql key key.enc payload.*
+echo ""
 
 # finish
 cd
+echo "Finished"
+echo ""
